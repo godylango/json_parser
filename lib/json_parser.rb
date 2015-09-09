93d19bd95295b0538b28d9343e248931e517f14b
Dylan Clark <dylan@indiegogo.com>
2015-09-08 19:20:57 -0700

    Move JsonParser Module to Class

--- lib/json_parser.rb
+++ lib/json_parser.rb
@@ -1,11 +1,22 @@
-module JsonParser
+class JsonParser
   def self.parse(input)
+    new(input).parse
+  end
+
+  def initialize(input)
     if input == '[]'
-      []
+      self.parsed_input = []
     elsif input == '{}'
-      {}
+      self.parsed_input = {}
     else
-      nil
+      self.parsed_input = nil
     end
   end
+
+  def parse
+    parsed_input
+  end
+
+  private
+  attr_accessor :parsed_input
 end
