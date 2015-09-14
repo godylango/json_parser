5d4892acedd60b14bc75d8b07de57cf158c988f4
Dylan Clark <dylan@indiegogo.com>
2015-09-13 18:41:26 -0700

    Refactor Tokenizer.

--- lib/json_parser/tokenizer.rb
+++ lib/json_parser/tokenizer.rb
@@ -1,21 +1,39 @@
 class JsonParser
   class Tokenizer
     def self.tokenize(input)
-      @scanner = StringScanner.new(input)
-      tokens = []
+      tokenizer = Tokenizer.new(input)
+      tokenizer.tokens
+    end
 
-      until @scanner.eos?
-        matchers.detect do |type, matcher|
-          if @scanner.scan(matcher)
-            tokens << [type, @scanner.matched]
-          end
-        end
+    attr_reader :tokens
+
+    def initialize(input)
+      @scanner = StringScanner.new(input)
+      @tokens = []
+      tokenize
+    end
+
+    private
+
+    def tokenize
+      until scanner.eos?
+        tokens << next_token
       end
 
       tokens
     end
 
-    def self.matchers
+    def next_token
+      [].tap do |next_token|
+        matchers.detect do |type, matcher|
+          if scanner.scan(matcher)
+            next_token << type << scanner.matched
+          end
+        end
+      end
+    end
+
+    def matchers
       [[:array, /[\[\]]/],
        [:number, /\d+/],
        [:string, /"(?:[^"\\]|\\.)*"/],
@@ -29,7 +47,6 @@ class JsonParser
        [:unknown, /./]]
     end
 
-    private
     attr_reader :scanner
   end
 end
