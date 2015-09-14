5f95ff6563611afbebe38958007b88d65d5f2604
Dylan Clark <dylan@indiegogo.com>
2015-09-14 00:51:54 -0700

    Extract TreeConstructor class

--- lib/json_parser.rb
+++ lib/json_parser.rb
@@ -4,13 +4,16 @@ class JsonParser
   end
 
   def initialize(input)
-    @tokens = Tokenizer.tokenize(input)
-    @root = get_root
+    tokens = Tokenizer.tokenize(input)
+
+    parsed_tokens = tokens.map do |token|
+      TokenParser.new(token)
+    end
+
+    @root = TreeConstructor.new(parsed_tokens).root
   end
 
   def parse
-    construct_tree
-
     if root
       root.value
     end
@@ -19,35 +22,6 @@ class JsonParser
   private
 
   attr_reader :tokens, :root
-
-  def construct_tree
-    pointer = root
-
-    tokens.each do |token|
-      parsed = TokenParser.new(token)
-      if parsed.should_push?
-        pointer.push(parsed.node)
-      end
-
-      if parsed.should_move_down?
-        pointer = parsed.node
-      elsif parsed.should_move_up?
-        pointer = pointer.parent
-      end
-    end
-  end
-
-  def get_root
-    first_token = tokens.shift
-    if first_token
-      tokens.pop
-      if first_token.first == :array
-        ArrayNode.new
-      elsif first_token.first == :object
-        ObjectNode.new
-      end
-    end
-  end
 end
 
 require 'requirements'
--- lib/requirements.rb
+++ lib/requirements.rb
@@ -1,4 +1,5 @@
 require 'json_parser/token_parser'
+require 'json_parser/tree_constructor'
 require 'json_parser/tokenizer'
 require 'json_parser/value_node'
 require 'json_parser/array_node'
